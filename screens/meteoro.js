3a9103480ba3f3e5de126067f96c65036521d52b
PriceWise <73356785+pricemastertab@users.noreply.github.com>
2023-08-29 19:28:29 -0300

    Add files via upload

---
+++ screens/meteoro.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function meteoScreen() {
+    return (
+      <>
+        <View style={{flex:1,
+                    justifyContent:'center',
+                    alignItems:'center'}}>
+          <Text>Tela de meteoros</Text>
+        </View>
+      </>
+    );
+  }
